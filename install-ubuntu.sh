79b877580aad7e48e125db50858afabaf1e0011f
Jore <jorejerry0@gmail.com>
2025-05-15 01:30:47 +0300

    Major Update: Set up GUI during installation.
    Other improvements
    - Update kernel version
    - Update messages

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -22,19 +22,23 @@
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.   #
 #                                                                              #
 ################################################################################
-# shellcheck disable=SC2034
+# shellcheck disable=SC2034,SC2155
 
 # ATTENTION!!! CHANGE BELOW FUNTIONS FOR DISTRO DEPENDENT ACTIONS!!!
 
-# Called before any safety checks
-# New Variables: AUTHOR GITHUB LOG_FILE ACTION_INSTALL ACTION_CONFIGURE
-#                ROOTFS_DIRECTORY COLOR_SUPPORT all_available_colors
+################################################################################
+# Called before any safety checks                                              #
+# New Variables: AUTHOR GITHUB LOG_FILE ACTION_INSTALL ACTION_CONFIGURE        #
+#                ROOTFS_DIRECTORY COLOR_SUPPORT all_available_colors           #
+################################################################################
 pre_check_actions() {
 	return
 }
 
-# Called before printing intro
-# New Variables: none
+################################################################################
+# Called before printing intro                                                 #
+# New Variables: none                                                          #
+################################################################################
 distro_banner() {
 	local spaces=''
 	for ((i = $((($(stty size | cut -d ' ' -f2) - 40) / 2)); i > 0; i--)); do
@@ -63,88 +67,42 @@ distro_banner() {
 	msg -a "${spaces}    ${DISTRO_NAME} ${Y}${VERSION_NAME}"
 }
 
-# Called after checking architecture and required pkgs
-# New Variables: SYS_ARCH LIB_GCC_PATH
+################################################################################
+# Called after checking architecture and required pkgs                         #
+# New Variables: SYS_ARCH LIB_GCC_PATH                                         #
+################################################################################
 post_check_actions() {
 	return
 }
 
-# Called after checking for rootfs directory
-# New Variables: KEEP_ROOTFS_DIRECTORY
+################################################################################
+# Called after checking for rootfs directory                                   #
+# New Variables: KEEP_ROOTFS_DIRECTORY                                         #
+################################################################################
 pre_install_actions() {
 	ARCHIVE_NAME="${code_name}-server-cloudimg-${SYS_ARCH}-root.tar.xz"
 }
 
-# Called after extracting rootfs
-# New Variables: KEEP_ROOTFS_ARCHIVE
+################################################################################
+# Called after extracting rootfs                                               #
+# New Variables: KEEP_ROOTFS_ARCHIVE                                           #
+################################################################################
 post_install_actions() {
-	msg -t "Lemme create an xstartup script for vnc."
-	local xstartup="$(
-		# Customize depending on distribution defaults
-		cat 2>>"${LOG_FILE}" <<-EOF
-			#!/bin/bash
-			#############################
-			##          All            ##
-			unset SESSION_MANAGER
-			unset DBUS_SESSION_BUS_ADDRESS
-
-			export XDG_RUNTIME_DIR=/tmp/runtime-"\${USER:-root}"
-			export SHELL="\${SHELL:-/bin/sh}"
-
-			if [ -r ~/.Xresources ]; then
-			    xrdb ~/.Xresources
-			fi
-
-			#############################
-			##          Gnome          ##
-			# exec gnome-session
-
-			############################
-			##           LXQT         ##
-			# exec startlxqt
-
-			############################
-			##          KDE           ##
-			# exec startplasma-x11
-
-			############################
-			##          XFCE          ##
-			export QT_QPA_PLATFORMTHEME=qt5ct
-			exec startxfce4
-
-			############################
-			##           i3           ##
-			# exec i3
-
-			############################
-			##        BLACKBOX        ##
-			# exec blackbox
-		EOF
-	)"
-	if {
-		mkdir -p "${ROOTFS_DIRECTORY}/root/.vnc"
-		echo "${xstartup}" >"${ROOTFS_DIRECTORY}/root/.vnc/xstartup"
-		chmod 744 "${ROOTFS_DIRECTORY}/root/.vnc/xstartup"
-		if [ "${DEFAULT_LOGIN}" != "root" ]; then
-			mkdir -p "${ROOTFS_DIRECTORY}/home/${DEFAULT_LOGIN}/.vnc"
-			echo "${xstartup}" >"${ROOTFS_DIRECTORY}/home/${DEFAULT_LOGIN}/.vnc/xstartup"
-			chmod 744 "${ROOTFS_DIRECTORY}/home/${DEFAULT_LOGIN}/.vnc/xstartup"
-		fi
-	} 2>>"${LOG_FILE}"; then
-		msg -s "Done, xstartup script created successfully!"
-	else
-		msg -e "Sorry, I failed to create the xstartup script for vnc."
-	fi
+	return
 }
 
-# Called before making configurations
-# New Variables: none
+################################################################################
+# Called before making configurations                                          #
+# New Variables: none                                                          #
+################################################################################
 pre_config_actions() {
 	mkdir -p "${ROOTFS_DIRECTORY}/etc" >>"${LOG_FILE}" 2>&1 && echo "${ROOTFS_DIRECTORY}" >"${ROOTFS_DIRECTORY}/etc/debian_chroot"
 }
 
-# Called after configurations
-# New Variables: none
+################################################################################
+# Called after configurations                                                  #
+# New Variables: none                                                          #
+################################################################################
 post_config_actions() {
 	# execute distro specific command for locale generation
 	if [ -f "${ROOTFS_DIRECTORY}/etc/locale.gen" ] && [ -x "${ROOTFS_DIRECTORY}/sbin/dpkg-reconfigure" ]; then
@@ -153,28 +111,121 @@ post_config_actions() {
 		if distro_exec DEBIAN_FRONTEND=noninteractive /sbin/dpkg-reconfigure locales >>"${LOG_FILE}" 2>&1; then
 			msg -s "Done, the locales are ready!"
 		else
-			msg -e "Sorry, I failed to generate the locales."
+			msg -e "I failed to generate the locales."
 		fi
 	fi
 }
 
-# Called before complete message
-# New Variables: none
+################################################################################
+# Called before complete message                                               #
+# New Variables: none                                                          #
+################################################################################
 pre_complete_actions() {
-	return
-}
-
-# Called after complete message
-# New Variables: none
-post_complete_actions() {
-	if ${ACTION_INSTALL}; then
-		msg -te "Remember, this is a minimal installation of ${DISTRO_NAME}."
-		msg "If you need to install additional packages, check out the documentation for a guide."
+	if ! ${GUI_INSTALLED:-false} && ask -y -- -t "Should I set up the GUI now?"; then
+		set_up_gui && set_up_browser && GUI_INSTALLED=true
 	fi
 }
 
-# These variables are
-# only used in this script:
+################################################################################
+# Called after complete message                                                #
+# New Variables: none                                                          #
+################################################################################
+post_complete_actions() {
+	return
+}
+
+################################################################################
+# Local Functions                                                              #
+################################################################################
+
+# Sets up the GUI
+set_up_gui() {
+	msg -t "The installation is going to take very long."
+	msg "Lemme me acquire the '${Y}Termux wake lock${C}'."
+	if [ -x "$(command -v termux-wake-lock)" ]; then
+		if termux-wake-lock >>"${LOG_FILE}" 2>&1; then
+			msg -s "Great, the Termux wake lock is now activated."
+		else
+			msg -e "I have failed to set up the Termux wake lock."
+			msg "Keep Termux open during the installation."
+		fi
+	else
+		msg -e "I could not find the '${Y}termux-wake-lock${R}' command."
+		msg "Keep Termux open during the installation."
+	fi
+	msg -t "Lemme first upgrade the packages in ${DISTRO_NAME}."
+	msg "This won't take long."
+	if distro_exec apt update && distro_exec apt full-upgrade; then
+		msg -s "Done, all the ${DISTRO_NAME} packages are upgraded."
+		msg -t "Now lemme install the GUI in ${DISTRO_NAME}."
+		msg "This will take very long."
+		if distro_exec apt install -y tigervnc-standalone-server dbus-x11 ubuntu-desktop; then
+			msg -s "Finally, the GUI is now installed in ${DISTRO_NAME}."
+			msg -t "Now lemme add the xstartup script for VNC."
+			if {
+				local xstartup="$(
+					cat 2>>"${LOG_FILE}" <<-EOF
+						#!/usr/bin/bash
+						unset SESSION_MANAGER
+						unset DBUS_SESSION_BUS_ADDRESS
+						export XDG_RUNTIME_DIR=\${TMPDIR:-/tmp}/runtime-"\${USER:-root}"
+						export SHELL="\${SHELL:-/bin/sh}"
+						if [ -r ~/.Xresources ]; then
+						    xrdb ~/.Xresources
+						fi
+						exec gnome-session
+					EOF
+				)"
+				mkdir -p "${ROOTFS_DIRECTORY}/root/.vnc"
+				echo "${xstartup}" >"${ROOTFS_DIRECTORY}/root/.vnc/xstartup"
+				chmod 744 "${ROOTFS_DIRECTORY}/root/.vnc/xstartup"
+				if [ "${DEFAULT_LOGIN}" != "root" ]; then
+					mkdir -p "${ROOTFS_DIRECTORY}/home/${DEFAULT_LOGIN}/.vnc"
+					echo "${xstartup}" >"${ROOTFS_DIRECTORY}/home/${DEFAULT_LOGIN}/.vnc/xstartup"
+					chmod 744 "${ROOTFS_DIRECTORY}/home/${DEFAULT_LOGIN}/.vnc/xstartup"
+				fi
+			} 2>>"${LOG_FILE}"; then
+				msg -s "Done, xstartup script added successfully!"
+			else
+				msg -e "I failed to add the xstartup script."
+			fi
+		else
+			msg -qm0 "I have failed to install the GUI in ${DISTRO_NAME}."
+		fi
+	else
+		msg -qm0 "I have failed to upgrade the packages in ${DISTRO_NAME}."
+	fi
+}
+
+# Sets up the Browser
+set_up_browser() {
+	local available_browsers=(
+		"Chromium" "Firefox" "Both Browsers"
+	)
+	choose -d2 -t "Select your prefered Browser." \
+		"${available_browsers[@]}"
+	local selected_browser="${available_browsers[$((${?} - 1))]}"
+	local selected_browsers verb
+	if [ "${selected_browser}" = "${available_browsers[-1]}" ]; then
+		selected_browsers=("${available_browsers[@]:0:${#available_browsers[@]}-1}")
+		selected_browsers=("${selected_browsers[@]// /-}")
+		verb="are"
+	else
+		selected_browsers=("${selected_browser// /-}")
+		verb="is"
+	fi
+	msg "Okay then, I shall install '${Y}${selected_browser}${C}'."
+	if distro_exec apt install -y "${selected_browsers[@],,}"; then
+		if [ "${selected_browsers[0]}" = "${available_browsers[0]}" ]; then
+			sed -Ei 's/^(Exec=.*chromium).*(%U)$/\1 --no-sandbox \2/' "${ROOTFS_DIRECTORY}/usr/share/applications/chromium.desktop"
+		fi
+		msg "Done, ${selected_browser} ${verb} now installed in ${DISTRO_NAME}."
+	else
+		msg -e "I have failed to install ${selected_browser} in ${DISTRO_NAME}."
+	fi
+}
+
+# These variables are only used in this script:
 # 		name, code_name, release
 
 name="22.04 LTS"
@@ -185,6 +236,7 @@ DISTRO_NAME="Ubuntu"
 PROGRAM_NAME="$(basename "${0}")"
 DISTRO_REPOSITORY="termux-ubuntu"
 VERSION_NAME="${name} ${code_name}-${release}"
+KERNEL_RELEASE="${DISTRO_NAME// /-}-${VERSION_NAME// /-}-proot"
 
 SHASUM_CMD=sha256sum
 TRUSTED_SHASUMS="$(
@@ -195,9 +247,7 @@ TRUSTED_SHASUMS="$(
 )"
 
 ARCHIVE_STRIP_DIRS=0 # directories stripped by tar when extracting rootfs archive
-KERNEL_RELEASE="6.2.1-ubuntu-proot"
 BASE_URL="https://cloud-images.ubuntu.com/${code_name}/${release}"
-
 TERMUX_FILES_DIR="/data/data/com.termux/files"
 
 DISTRO_SHORTCUT="${TERMUX_FILES_DIR}/usr/bin/ub"
