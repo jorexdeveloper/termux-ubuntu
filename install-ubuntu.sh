8682d36de11d3de959706f00524eadd02516305f
Jore <jorejerry0@gmail.com>
2025-11-20 09:14:15 +0300

    Major update: update installer to work with new termux-distro.sh changes.
    NOTE: Version now incompatible with previous commits
    
    New Features
    - New advanced TUI
    - Implemented separate terminal buffer (for package installation, etc)
    - Implemented animations
    - It's easier now to customize colors
    
    Other changes
    - Code refactor
    - Bug fixes and improvements
      - Might finally fix the package dependencies problem permanently
      - Allow further installation when desktop installation fails
    
    For detailed changes, see https://github.com/jorexdeveloper/termux-distro/compare/ffdf9f78cd554c1c1c5c77f3117f7ba555974493...fb9c35b4d664e1230155e0db469c330de005e54a

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -2,24 +2,24 @@
 
 ################################################################################
 #                                                                              #
-#     Termux Ubuntu Installer.                                                 #
+# Termux Ubuntu Installer.                                                     #
 #                                                                              #
-#     Installs Ubuntu in Termux.                                               #
+# Installs Ubuntu in Termux.                                                   #
 #                                                                              #
-#     Copyright (C) 2023-2025  Jore <https://github.com/jorexdeveloper>        #
+# Copyright (C) 2023-2025  Jore <https://github.com/jorexdeveloper>            #
 #                                                                              #
-#     This program is free software: you can redistribute it and/or modify     #
-#     it under the terms of the GNU General Public License as published by     #
-#     the Free Software Foundation, either version 3 of the License, or        #
-#     (at your option) any later version.                                      #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
 #                                                                              #
-#     This program is distributed in the hope that it will be useful,          #
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of           #
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
-#     GNU General Public License for more details.                             #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
 #                                                                              #
-#     You should have received a copy of the GNU General Public License        #
-#     along with this program.  If not, see <https://www.gnu.org/licenses/>.   #
+# You should have received a copy of the GNU General Public License            #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.       #
 #                                                                              #
 ################################################################################
 # shellcheck disable=SC2034,SC2155
@@ -29,10 +29,12 @@
 ################################################################################
 # Called before any safety checks                                              #
 # New Variables: AUTHOR GITHUB LOG_FILE ACTION_INSTALL ACTION_CONFIGURE        #
-#                ROOTFS_DIRECTORY COLOR_SUPPORT all_available_colors           #
+#                ROOTFS_DIRECTORY COLOR_SUPPORT (all available colors)         #
 ################################################################################
 pre_check_actions() {
-	return
+	P=${W} # primary color
+	S=${R} # secondary color
+	T=${M} # tertiary color
 }
 
 ################################################################################
@@ -40,31 +42,28 @@ pre_check_actions() {
 # New Variables: none                                                          #
 ################################################################################
 distro_banner() {
-	local spaces=''
-	for ((i = $((($(stty size | cut -d ' ' -f2) - 40) / 2)); i > 0; i--)); do
-		spaces+=' '
-	done
-	msg -a "${spaces}${R}            .-/+oossssoo+/-."
-	msg -a "${spaces}${R}        ':+ssssssssssssssssss+:'"
-	msg -a "${spaces}${R}      -+ssssssssssssssssssyyssss+-"
-	msg -a "${spaces}${R}    .osssssssssssssssss ${N}dMMMN ${R}sssso."
-	msg -a "${spaces}${R}   /ssssssssss ${N}hdmmNNmmyNMMMM ${R}ssssss/"
-	msg -a "${spaces}${R}  +ssssssss h y ${N}MMMMMMMNdddd ${R}ssssssss+"
-	msg -a "${spaces}${R} /sssssss ${N}hNMM ${R}y ${N}hyyyyhmNMMMN ${R}ssssssss/"
-	msg -a "${spaces}${R}.sssssss ${N}dMMMN ${R}sssssssss ${N}hNMMM ${R}ssssssss."
-	msg -a "${spaces}${R}+sss ${N}hhhyNMMN ${R}sssssssssss ${N}yNMMM ${R}sssssss+"
-	msg -a "${spaces}${R}os ${N}yNMMMNyMM ${R}sssssssssssss ${N}hmmm ${R}ssssssso"
-	msg -a "${spaces}${R}os ${N}yNMMMNyMM ${R}sssssssssssssshmmmh${R}ssssssso"
-	msg -a "${spaces}${R}+sss ${N}hhhyNMMN ${R}sssssssssss ${N}yNMMM ${R}sssssss+"
-	msg -a "${spaces}${R}.sssssss ${N}dMMMN ${R}sssssssss ${N}hNMMM ${R}ssssssss."
-	msg -a "${spaces}${R} /sssssss ${N}hNMM ${R}y ${N}hyyyyhdNMMMN ${R}ssssssss/"
-	msg -a "${spaces}${R}  +ssssssss d y ${N}MMMMMMMMdddd ${R}ssssssss+"
-	msg -a "${spaces}${R}   /ssssssssss ${N}hdmNNNNmyNMMMM ${R}ssssss/"
-	msg -a "${spaces}${R}    .osssssssssssssssss ${N}dMMMN ${R}sssso."
-	msg -a "${spaces}${R}      -+ssssssssssssssss ${N}yy ${R}ssss+-"
-	msg -a "${spaces}${R}        ':+ssssssssssssssssss+:'"
-	msg -a "${spaces}${R}            .-/+oossssoo+/-."
-	msg -a "${spaces}    ${DISTRO_NAME} ${Y}${VERSION_NAME}"
+	local spaces=$(printf "%*s" $((($(stty size | awk '{print $2}') - 40) / 2)) "")
+	msg -a "${spaces}${S}            .-/+oossssoo+/-."
+	msg -a "${spaces}${S}        ':+ssssssssssssssssss+:'"
+	msg -a "${spaces}${S}      -+ssssssssssssssssssyyssss+-"
+	msg -a "${spaces}${S}    .osssssssssssssssss ${N}dMMMN ${S}sssso."
+	msg -a "${spaces}${S}   /ssssssssss ${N}hdmmNNmmyNMMMM ${S}ssssss/"
+	msg -a "${spaces}${S}  +ssssssss h y ${N}MMMMMMMNdddd ${S}ssssssss+"
+	msg -a "${spaces}${S} /sssssss ${N}hNMM ${S}y ${N}hyyyyhmNMMMN ${S}ssssssss/"
+	msg -a "${spaces}${S}.sssssss ${N}dMMMN ${S}sssssssss ${N}hNMMM ${S}ssssssss."
+	msg -a "${spaces}${S}+sss ${N}hhhyNMMN ${S}sssssssssss ${N}yNMMM ${S}sssssss+"
+	msg -a "${spaces}${S}os ${N}yNMMMNyMM ${S}sssssssssssss ${N}hmmm ${S}ssssssso"
+	msg -a "${spaces}${S}os ${N}yNMMMNyMM ${S}sssssssssssssshmmmh${S}ssssssso"
+	msg -a "${spaces}${S}+sss ${N}hhhyNMMN ${S}sssssssssss ${N}yNMMM ${S}sssssss+"
+	msg -a "${spaces}${S}.sssssss ${N}dMMMN ${S}sssssssss ${N}hNMMM ${S}ssssssss."
+	msg -a "${spaces}${S} /sssssss ${N}hNMM ${S}y ${N}hyyyyhdNMMMN ${S}ssssssss/"
+	msg -a "${spaces}${S}  +ssssssss d y ${N}MMMMMMMMdddd ${S}ssssssss+"
+	msg -a "${spaces}${S}   /ssssssssss ${N}hdmNNNNmyNMMMM ${S}ssssss/"
+	msg -a "${spaces}${S}    .osssssssssssssssss ${N}dMMMN ${S}sssso."
+	msg -a "${spaces}${S}      -+ssssssssssssssss ${N}yy ${S}ssss+-"
+	msg -a "${spaces}${S}        ':+ssssssssssssssssss+:'"
+	msg -a "${spaces}${S}            .-/+oossssoo+/-."
+	msg -a "${spaces}    ${P}${DISTRO_NAME}${S} ${T}${VERSION_NAME}${S}"
 }
 
 ################################################################################
@@ -80,7 +79,7 @@ post_check_actions() {
 # New Variables: KEEP_ROOTFS_DIRECTORY                                         #
 ################################################################################
 pre_install_actions() {
-	ARCHIVE_NAME="${code_name}-server-cloudimg-${SYS_ARCH}-root.tar.xz"
+	ARCHIVE_NAME=${code_name}-server-cloudimg-${SYS_ARCH}-root.tar.xz
 }
 
 ################################################################################
@@ -96,7 +95,7 @@ post_install_actions() {
 # New Variables: none                                                          #
 ################################################################################
 pre_config_actions() {
-	mkdir -p "${ROOTFS_DIRECTORY}/etc" >>"${LOG_FILE}" 2>&1 && echo "${ROOTFS_DIRECTORY}" >"${ROOTFS_DIRECTORY}/etc/debian_chroot"
+	mkdir -p "${ROOTFS_DIRECTORY}"/etc &>>"${LOG_FILE}" && echo "${ROOTFS_DIRECTORY}" >"${ROOTFS_DIRECTORY}"/etc/debian_chroot
 }
 
 ################################################################################
@@ -104,14 +103,16 @@ pre_config_actions() {
 # New Variables: none                                                          #
 ################################################################################
 post_config_actions() {
-	# execute distro specific command for locale generation
-	if [ -f "${ROOTFS_DIRECTORY}/etc/locale.gen" ] && [ -x "${ROOTFS_DIRECTORY}/sbin/dpkg-reconfigure" ]; then
-		msg -t "Hold on while I generate the locales for you."
-		sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' "${ROOTFS_DIRECTORY}/etc/locale.gen"
-		if distro_exec DEBIAN_FRONTEND=noninteractive /sbin/dpkg-reconfigure locales >>"${LOG_FILE}" 2>&1; then
-			msg -s "Done, the locales are ready!"
+	if [[ -f ${ROOTFS_DIRECTORY}/etc/locale.gen && -x ${ROOTFS_DIRECTORY}/sbin/dpkg-reconfigure ]]; then
+		msg -tn "Generating locales..."
+		sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' "${ROOTFS_DIRECTORY}"/etc/locale.gen
+
+		if distro_exec DEBIAN_FRONTEND=noninteractive /sbin/dpkg-reconfigure locales &>>"${LOG_FILE}"; then
+			cursor -u1
+			msg -ts "Locales generated"
 		else
-			msg -e "I failed to generate the locales."
+			cursor -u1
+			msg -te "Failed to generate locales."
 		fi
 	fi
 }
@@ -121,8 +122,11 @@ post_config_actions() {
 # New Variables: none                                                          #
 ################################################################################
 pre_complete_actions() {
-	if ! ${GUI_INSTALLED:-false} && ask -y -- -t "Should I set up the GUI now?"; then
-		set_up_gui && set_up_browser && GUI_INSTALLED=true
+	if [[ ! ${DE_INSTALLED} ]] && ask -y -- -t "Install Desktop Environment?"; then
+		set_up_de && {
+			DE_INSTALLED=1
+			set_up_browser
+		}
 	fi
 }
 
@@ -138,133 +142,154 @@ post_complete_actions() {
 # Local Functions                                                              #
 ################################################################################
 
-# Sets up the GUI
-set_up_gui() {
-	msg -t "The installation is going to take very long."
-	msg "Lemme me acquire the '${Y}Termux wake lock${C}'."
-	if [ -x "$(command -v termux-wake-lock)" ]; then
-		if termux-wake-lock >>"${LOG_FILE}" 2>&1; then
-			msg -s "Great, the Termux wake lock is now activated."
+# Sets up the desktop environment
+set_up_de() {
+	if command -v termux-wake-lock &>>"${LOG_FILE}"; then
+		msg -tn "Acquiring Termux wake lock..."
+
+		if termux-wake-lock &>>"${LOG_FILE}"; then
+			cursor -u1
+			msg -ts "Termux wake lock held"
 		else
-			msg -e "I have failed to set up the Termux wake lock."
-			msg "Keep Termux open during the installation."
+			cursor -u1
+			msg -te "Failed to acquire Termux wake lock"
 		fi
-	else
-		msg -e "I could not find the '${Y}termux-wake-lock${R}' command."
-		msg "Keep Termux open during the installation."
 	fi
-	msg -t "Lemme first upgrade the packages in ${DISTRO_NAME}."
-	msg "This won't take long."
-	if distro_exec apt update && distro_exec apt full-upgrade; then
-		msg -s "Done, all the ${DISTRO_NAME} packages are upgraded."
-		msg -t "Now lemme install the GUI in ${DISTRO_NAME}."
-		msg "This will take very long."
-		if distro_exec apt install -y tigervnc-standalone-server dbus-x11 ubuntu-desktop; then
-			msg -s "Finally, the GUI is now installed in ${DISTRO_NAME}."
-			msg -t "Now lemme add the xstartup script for VNC."
-			if {
-				local xstartup="$(
-					cat 2>>"${LOG_FILE}" <<-EOF
-						#!/usr/bin/bash
-						unset SESSION_MANAGER
-						unset DBUS_SESSION_BUS_ADDRESS
-						export XDG_RUNTIME_DIR=\${TMPDIR:-/tmp}/runtime-"\${USER:-root}"
-						export SHELL="\${SHELL:-/bin/sh}"
-						if [ -r ~/.Xresources ]; then
-						    xrdb ~/.Xresources
-						fi
-						exec gnome-session
-					EOF
-				)"
-				mkdir -p "${ROOTFS_DIRECTORY}/root/.vnc"
-				echo "${xstartup}" >"${ROOTFS_DIRECTORY}/root/.vnc/xstartup"
-				chmod 744 "${ROOTFS_DIRECTORY}/root/.vnc/xstartup"
-				if [ "${DEFAULT_LOGIN}" != "root" ]; then
-					mkdir -p "${ROOTFS_DIRECTORY}/home/${DEFAULT_LOGIN}/.vnc"
-					echo "${xstartup}" >"${ROOTFS_DIRECTORY}/home/${DEFAULT_LOGIN}/.vnc/xstartup"
-					chmod 744 "${ROOTFS_DIRECTORY}/home/${DEFAULT_LOGIN}/.vnc/xstartup"
+
+	msg -tn "Installing desktop packages in ${DISTRO_NAME}..."
+	trap 'buffer -h; echo; msg -fem2; exit 130' INT
+	buffer -s
+
+	local pkgs=(tigervnc-standalone-server dbus-x11 ubuntu-desktop)
+	if buffer -i apt update && distro_exec apt update &&
+		buffer -i apt full-upgrade && distro_exec apt full-upgrade &&
+		buffer -i apt install -y "${pkgs[@]}" && distro_exec apt install -y "${pkgs[@]}"; then
+		buffer -h3
+		trap - INT
+		cursor -u1
+		msg -ts "Desktop packages installed in ${DISTRO_NAME}"
+
+		msg -tn "Creating xstartup program..."
+
+		local xstartup=$(
+			cat 2>>"${LOG_FILE}" <<-EOF
+				#!/bin/bash
+				unset SESSION_MANAGER
+				unset DBUS_SESSION_BUS_ADDRESS
+
+				export XDG_RUNTIME_DIR=\${TMPDIR}/runtime-"\${USER}"
+				export SHELL=\${SHELL}
+
+				if [[ -r ~/.Xresources ]]; then
+				    xrdb ~/.Xresources
 				fi
-			} 2>>"${LOG_FILE}"; then
-				msg -s "Done, xstartup script added successfully!"
-			else
-				msg -e "I failed to add the xstartup script."
-			fi
+
+				exec gnome-session
+			EOF
+		)
+
+		if {
+			mkdir -p "${ROOTFS_DIRECTORY}"/root/.vnc &&
+				echo "${xstartup}" >"${ROOTFS_DIRECTORY}"/root/.vnc/xstartup &&
+				chmod 744 "${ROOTFS_DIRECTORY}"/root/.vnc/xstartup &&
+				if [[ ${DEFAULT_LOGIN} != root ]]; then
+					mkdir -p "${ROOTFS_DIRECTORY}"/home/"${DEFAULT_LOGIN}"/.vnc &&
+						echo "${xstartup}" >"${ROOTFS_DIRECTORY}"/home/"${DEFAULT_LOGIN}"/.vnc/xstartup &&
+						chmod 744 "${ROOTFS_DIRECTORY}"/home/"${DEFAULT_LOGIN}"/.vnc/xstartup
+				fi
+		} 2>>"${LOG_FILE}"; then
+			cursor -u1
+			msg -ts "Xstartup program created"
 		else
-			msg -qm0 "I have failed to install the GUI in ${DISTRO_NAME}."
+			cursor -u1
+			msg -te "Failed create xstartup program"
 		fi
 	else
-		msg -qm0 "I have failed to upgrade the packages in ${DISTRO_NAME}."
+		buffer -h5
+		trap - INT
+		cursor -u1
+		msg -te "Failed to install Desktop packages in ${DISTRO_NAME}"
+		return 1
 	fi
 }
 
 # Sets up the Browser
 set_up_browser() {
-	local available_browsers=(
-		"Chromium" "Firefox" "Both Browsers"
+	local available_browsers selected_browser selected_browsers suffix
+	available_browsers=(
+		"Chromium" "Firefox" "Chromium & Firefox"
 	)
-	choose -d2 -t "Select your prefered Browser." \
+
+	choose -d2 -t "Select Browser" \
 		"${available_browsers[@]}"
-	local selected_browser="${available_browsers[$((${?} - 1))]}"
-	local selected_browsers verb
-	if [ "${selected_browser}" = "${available_browsers[-1]}" ]; then
+	selected_browser=${available_browsers[$((${?} - 1))]}
+
+	if [[ ${selected_browser} == "${available_browsers[-1]}" ]]; then
 		selected_browsers=("${available_browsers[@]:0:${#available_browsers[@]}-1}")
 		selected_browsers=("${selected_browsers[@]// /-}")
-		verb="are"
+		suffix=s
 	else
 		selected_browsers=("${selected_browser// /-}")
-		verb="is"
+		suffix=
 	fi
-	msg "Okay then, I shall install '${Y}${selected_browser}${C}'."
-	if distro_exec apt install -y "${selected_browsers[@],,}"; then
-		if [ "${selected_browsers[0]}" = "${available_browsers[0]}" ]; then
-			sed -Ei 's/^(Exec=.*chromium).*(%U)$/\1 --no-sandbox \2/' "${ROOTFS_DIRECTORY}/usr/share/applications/chromium.desktop"
+
+	msg -tn "Installing ${selected_browser} Browser${suffix}..."
+	trap 'buffer -h; echo; msg -fem2; exit 130' INT
+	buffer -s
+
+	if buffer -i apt install -y "${selected_browsers[@],,}" && distro_exec apt install -y "${selected_browsers[@],,}"; then
+		if [[ ${selected_browsers[0]} == "${available_browsers[0]}" && -f "${ROOTFS_DIRECTORY}"/usr/share/applications/chromium.desktop ]]; then
+			sed -Ei 's/^(Exec=.*chromium).*(%U)$/\1 --no-sandbox \2/' "${ROOTFS_DIRECTORY}"/usr/share/applications/chromium.desktop
 		fi
-		msg "Done, ${selected_browser} ${verb} now installed in ${DISTRO_NAME}."
+
+		buffer -h3
+		trap - INT
+		cursor -u1
+		msg -ts "${selected_browser} Browser${suffix} installed"
 	else
-		msg -e "I have failed to install ${selected_browser} in ${DISTRO_NAME}."
+		buffer -h5
+		trap - INT
+		cursor -u1
+		msg -te "Failed to install ${selected_browser} Browser${suffix}"
 	fi
 }
 
-# These variables are only used in this script:
-# 		name, code_name, release
-
 name="22.04 LTS"
-code_name="noble"
-release="20251113"
+code_name=noble
+release=20251113
 
-DISTRO_NAME="Ubuntu"
-PROGRAM_NAME="$(basename "${0}")"
-DISTRO_REPOSITORY="termux-ubuntu"
+DISTRO_NAME=Ubuntu
+PROGRAM_NAME=$(basename "${0}")
+DISTRO_REPOSITORY=termux-ubuntu
+KERNEL_RELEASE=$(uname -r)
 VERSION_NAME="${name} ${code_name}-${release}"
-KERNEL_RELEASE="$(uname -r)"
 
 SHASUM_CMD=sha256sum
-TRUSTED_SHASUMS="$(
+TRUSTED_SHASUMS=$(
 	cat <<-EOF
 		35228d1774d20099887d764806e076089920ca9c0d0443e5b15d542b4b8cb554 *noble-server-cloudimg-armhf-root.tar.xz
 		ce9ffe737d8fa697f057e49b3f6dee57d5bce678c885e37ef11ff471c913bc1e *noble-server-cloudimg-arm64-root.tar.xz
 	EOF
-)"
+)
 
 ARCHIVE_STRIP_DIRS=0 # directories stripped by tar when extracting rootfs archive
-BASE_URL="https://cloud-images.ubuntu.com/${code_name}/${release}"
-TERMUX_FILES_DIR="/data/data/com.termux/files"
+BASE_URL=https://cloud-images.ubuntu.com/${code_name}/${release}
+TERMUX_FILES_DIR=/data/data/com.termux/files
 
-DISTRO_SHORTCUT="${TERMUX_FILES_DIR}/usr/bin/ub"
-DISTRO_LAUNCHER="${TERMUX_FILES_DIR}/usr/bin/ubuntu"
+DISTRO_SHORTCUT=${TERMUX_FILES_DIR}/usr/bin/ub
+DISTRO_LAUNCHER=${TERMUX_FILES_DIR}/usr/bin/ubuntu
 
-DEFAULT_ROOTFS_DIR="${TERMUX_FILES_DIR}/ubuntu"
-DEFAULT_LOGIN="root"
+DEFAULT_ROOTFS_DIR=${TERMUX_FILES_DIR}/ubuntu
+DEFAULT_LOGIN=root
 
 # WARNING!!! DO NOT CHANGE BELOW!!!
 
-# Check in script's directory for template
-distro_template="$(realpath "$(dirname "${0}")")/termux-distro.sh"
+# Check in program's directory for template
+distro_template=$(realpath "$(dirname "${0}")")/termux-distro.sh
+
 # shellcheck disable=SC1090
-if [ -f "${distro_template}" ] && [ -r "${distro_template}" ]; then
-	source "${distro_template}" "${@}"
-elif curl -fsSLO "https://raw.githubusercontent.com/jorexdeveloper/termux-distro/main/termux-distro.sh" 2>"/dev/null" && [ -f "${distro_template}" ]; then
-	source "${distro_template}"
+if [[ -f ${distro_template} ]] || curl -fsSLO https://raw.githubusercontent.com/jorexdeveloper/termux-distro/main/termux-distro.sh &>/dev/null; then
+	source "${distro_template}" "${@}" || exit 1
 else
-	echo "You need an active internet connection to run this script."
+	echo "You need an active internet connection to run this program."
 fi
